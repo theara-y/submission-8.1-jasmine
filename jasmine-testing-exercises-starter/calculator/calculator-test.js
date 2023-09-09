6375f475558c754faa73a22ec4af997b3925d81a
= <theara.ya@outlook.com>
2023-09-09 17:38:48 -0400

    removed comments

--- jasmine-testing-exercises-starter/calculator/calculator-test.js
+++ jasmine-testing-exercises-starter/calculator/calculator-test.js
@@ -1,16 +1,14 @@
 beforeEach(function() {
-  value = {amount: 100, years: 1, rate: 0.05};
-  zeroLoan = {amount: 0, years: 1, rate: 0.05};
+  mock0 = {amount: 0, years: 1, rate: 0.05};
+  mock1 = {amount: 100, years: 1, rate: 0.05};
 })
 
 it('should calculate the monthly rate correctly', function () {
-  // let value = {amount: 100, years: 1, rate: 0.05};
-  expect(calculateMonthlyPayment(value)).toEqual(8.56);
-  expect(calculateMonthlyPayment(zeroLoan)).toEqual(0.00);
+  expect(calculateMonthlyPayment(mock1)).toEqual(8.56);
+  expect(calculateMonthlyPayment(mock0)).toEqual(0.00);
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // let value = {amount: 100, years: 1, rate: 0.05};
-  expect(calculateMonthlyPayment(value)).toMatch(/\d+\.\d\d/);
+  expect(calculateMonthlyPayment(mock1)).toMatch(/\d+\.\d\d/);
 });
